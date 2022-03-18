6ef84d4c6f20ee150f8dcfae5c69b11072107abd
Aaron Langley <aaronlangley32@hotmail.com>
2022-03-17 20:41:41 -0400

    Started contactpage Mockup

---
+++ src/CSS/contactPageStyles.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+
+export const ContactContainer = styled.div`
+    height: 100%;
+    width: 100%;
+    background-color: red;
+`
+export const ContactHeader = styled.div`
+    text-align: center;
+    padding: 8rem 0rem 0rem 0rem;
+    font-size: 3rem;
+    color: #000000;
+    background-color: blue;
+    text-transform: capitalize;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+export const ContactContent = styled.div`
+    background-color: green;
+    height: 650px;
+    display: flex;
+    justify-content: center;
+    margin: 2rem 6rem 0rem 6rem;
+`
+export const ContentLeft = styled.div`
+    width: 650px;
+    height: 650px;
+    border-right: 1px solid black;
+    background-color: orange;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+export const ContentRight = styled.div`
+    width: 650px;
+    height: 650px;
+    background-color: darkblue;
+`
--- src/components/contactPage.js
+++ src/components/contactPage.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { ContactContainer, ContactContent, ContactHeader, ContentLeft, ContentRight } from '../CSS/contactPageStyles';
+
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <ContactContainer>
+      <ContactHeader>Hello World</ContactHeader>
+      <ContactContent>
+        <ContentLeft>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14188.779650577995!2d-80.2378521!3d27.2444157!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x519fddd59e7c8d5e!2sAble%20Closets!5e0!3m2!1sen!2sus!4v1647562211380!5m2!1sen!2sus" title="Showroom" width="600" height="450" style={{'border': 'none'}} allowFullScreen="" loading="lazy"></iframe>
+        </ContentLeft>
+        <ContentRight>
+
+        </ContentRight>
+      </ContactContent>
+    </ContactContainer>
   )
 }
 
